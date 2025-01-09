b8399a23aa36d25a27bc58550341a6a154505cc0
douniabadrane <douniaelmesbahy1996@gmail.com>
2025-01-09 23:15:02 +0100

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function number() 
+    {
+        $number = rand(0,100);
+
+    return $this->render('base.html.twig',["nini"=>$number]);
+
+    }
+}
+    
